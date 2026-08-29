9c805939c743ed66ac22a0c36eb7245c9b83deef
CK-Tot <changkuothtot41@gmail.com>
2026-08-28 21:10:04 -0500

    feat: import data

--- data/data.js
+++ data/data.js
@@ -1,1 +1,67 @@
-export const xData = [{}]
+export const xData = [
+    {
+        handle: '@ckthaengineer',
+        userName: 'CK Tot',
+        profilePic: 'images/profile-pic.jpg',
+        likes: 70,
+        retweets: 40,
+        tweetTxt: 'Hello World',
+        tweetImg: '/images/Profile-pic (15).jpg',
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '88f32190-9ebc-471f-831d-8043f0f2ae3a'
+    },
+    {
+        handle: "@jessthebest",
+        userName: 'Jess Will',
+        profilePic: 'images/Profile-pic (2).jpg',
+        likes: 40,
+        retweets: 10,
+        tweetTxt: "Does anyone know how to code in Html 🤔",
+        tweetImg: '',
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '617b3d3c-c451-4b45-9776-547313a823eb'
+    },
+    {
+        handle: '@mayacarter',
+        userName: 'Maya Carter',
+        profilePic: 'images/Profile-pic (3).jpg',
+        likes: 12,
+        retweets: 0,
+        tweetTxt: 'Coding partner? 🤓',
+        tweetImg: '',
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '9f391e6d-eba0-428a-9170-becb1f819f60'
+    },
+    {
+        handle: '@sophiabennett',
+        userName: 'Sophia Bennett',
+        profilePic: '/images/Profile-pic (4).jpg',
+        likes: 3,
+        retweets: 1,
+        tweetTxt: 'Test 🐣',
+        tweetImg: '',
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8a22467e-144f-482e-afe4-1b07f57711ea'
+    },
+    {
+        handle: '@marcusreed',
+        userName: 'Marcus Reed',
+        profilePic: '/images/Profile-pic (9).jpg',
+        likes: 20,
+        retweets: 10,
+        tweetTxt: 'Just got a new position! 🥳🙌🏾',
+        tweetImg: '',
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: 'eca3cf92-d7de-41e1-b072-a45aa50a7bf7'
+    }
+]
--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,130 @@
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 const inputBox = document.getElementById('input-box');
 const inputFile = document.getElementById('file-input');
 const imgPreview = document.querySelector('.img-preview');
+const tweetFeed = document.querySelector('.tweet-feed');
+import { xData } from "../data/data.js";
 
-inputFile.addEventListener('change', imageUpload);
 
-function imageUpload()
+// Get the img file and append it
+ inputFile.addEventListener('change', imageUpload);
+
+ function imageUpload()
+ {
+    const file = inputFile.files[0];
+
+    const imgLink = URL.createObjectURL(file);
+
+    imgPreview.innerHTML = `
+        <img src="${imgLink}" alt="Uploaded img">
+    `
+ }
+
+
+
+ /**
+  * <div class="tweet-content">
+                <div class="inner-container">
+                    <div class="tweet-header">
+                        <div class="img-container">
+                        <img src="images/profile-pic.jpg" alt="user handle" class="feed-pic">
+                    </div>
+                    <div class="user-info">
+                        <h3 class="user-name">CkThaEngineer<i class="fa-solid fa-circle-check"></i></h3>
+                        <span class="user-handle">@ckthaengineer</span>
+                    </div>
+                    </div>
+                    
+                    <div class="tweet-txt-info">
+                        <p class="tweet-text">Hello world 🌎 🙌🏾</p>
+                    </div>
+
+                    <div class="tweet-interactions">
+                        <span class="tweet-comment">
+                            <i class="fa-regular fa-comment"></i>
+                            12
+                        </span>
+                        <span class="tweet-rewteet">
+                           <i class="fa-solid fa-retweet"></i>
+                            3
+                        </span>
+                        <span class="tweet-like">
+                            <i class="fa-solid  fa-heart" ></i>
+                            12
+                        </span>
+                    </div>
+                    
+                </div>
+            </div>
+  * 
+  * 
+  */
+
+
+
+function render()
 {
-   const file = inputFile.files[0];
+    tweetFeed.innerHTML = "";
 
-   if (!file) return;
+    const feed = xData.map(tweet => {
+        let imgHtml = ``;
 
-   const imgLink = URL.createObjectURL(file);
-   imgPreview.innerHTML = `
-   <img src="${imgLink}" alt="uploaded img">
-   `
+        if (tweet.tweetImg)
+        {
+            imgHtml = `
+                <img 
+                    src="${tweet.tweetImg}" 
+                    class="tweet-img" 
+                    alt="tweet image"
+                >
+            `
+        }
 
+
+       return `
+            <div class="tweet-content">
+                <div class="inner-container">
+                    <div class="tweet-header">
+                        <div class="img-container">
+                        <img src="${tweet.profilePic}" alt="user handle" class="feed-pic">
+                    </div>
+                    <div class="user-info">
+                        <h3 class="user-name">${tweet.userName}<i class="fa-solid fa-circle-check"></i></h3>
+                        <span class="user-handle">${tweet.handle}</span>
+                    </div>
+                    </div>
+                    
+                    <div class="tweet-txt-info">
+                        <p class="tweet-text">${tweet.tweetTxt}</p>
+                        ${imgHtml}
+                    </div>
+
+                    <div class="tweet-interactions">
+                        <span class="tweet-comment">
+                            <i class="fa-regular fa-comment"></i>
+                           ${tweet.replies.length}
+                        </span>
+                        <span class="tweet-rewteet">
+                           <i class="fa-solid fa-retweet"></i>
+                           ${tweet.retweets}
+                        </span>
+                        <span class="tweet-like">
+                            <i class="fa-solid  fa-heart" ></i>
+                            ${tweet.likes}
+                        </span>
+                    </div>
+                    
+                </div>
+                <div class>
+                <p>replise test</p>
+                </div>
+            </div>
+        
+        
+        `
+    }).join(" ");
+
+    tweetFeed.innerHTML = feed;
 }
+
+render();
